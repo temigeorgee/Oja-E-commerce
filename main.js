5775f62c5fc2f64edd5d745482b63b80a23c4874
temigeorgee <george.temidayo@gmail.com>
2020-10-30 03:42:16 +0100

    changes on cart and nav

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
+//NAVBAR
+function classToggle() {
+    const navs = document.querySelectorAll('.navbarItems')
+    
+    navs.forEach(nav => nav.classList.toggle('navbarToggleShow'));
+  }
+  
+  document.querySelector('.navbarToggle').addEventListener('click', classToggle);
 
-var menuItems = document.getElementById("menuItems");
-menuItems.style.maxHeight = "0px";
+//LOGIN & REGISTER FORM
+var loginForm = document.getElementById("LoginForm");
+var regForm = document.getElementById("RegistrationForm");
+var Indicator = document.getElementById("Indicator");
 
-function menutoggle() {
-    if (menuItems.style.maxHeight == "0px") {
-        menuItems.style.maxHeight = "200px"
-    } else {
-        menuItems.style.maxHeight = "0px"
-    }
+function register() {
+    regForm.style.transform = "translateX(0px)";
+    loginForm.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(100px)";
 }
+
+function login() {
+    regForm.style.transform = "translateX(300px)";
+    loginForm.style.transform = "translateX(300px)";
+    Indicator.style.transform = "translateX(0px)";
+}
+// PRODUCTS
 let products = [
     {
         id : 1,
@@ -23,6 +38,7 @@ let products = [
         Long fluted Sleeves,
         Regular fit.</p>`,
         cost: 20,
+        inCart: 1,
         image_one: 'https://product.koovs.com/81981_57e16385de0cd38cefb03dd4604310e5.webp',
         image_two: 'https://product.koovs.com/81981_76e9db1e07d8bae7534bd2d2cb01d0c0.webp',
         image_three: 'https://product.koovs.com/81981_76e9db1e07d8bae7534bd2d2cb01d0c0.webp',
@@ -43,6 +59,7 @@ let products = [
         Long sleeves, 
         Slim fit.</p>`,
         cost: 20,
+        inCart: 1,
         image_one: 'https://product.koovs.com/155086_33b5919cf1a64a6485861ab43789142f_left_super_zoom.webp',
         image_two: 'https://product.koovs.com/155086_33b5919cf1a64a6485861ab43789142f_left_super_zoom.webp',
         image_three: 'https://product.koovs.com/155086_33b5919cf1a64a6485861ab43789142f_left_super_zoom.webp',
@@ -65,6 +82,7 @@ let products = [
         Long sleeves with cuffs
         Regular fit .</p>`,
         cost: 20,
+        inCart: 1,
         image_one: 'https://product.koovs.com/155084_8d808fbfb30043afb415a29b133c58d6_image1_super_zoom.webp',
         image_two: 'https://product.koovs.com/155084_8d808fbfb30043afb415a29b133c58d6_front_super_zoom.webp',
         image_three: 'https://product.koovs.com/155084_8d808fbfb30043afb415a29b133c58d6_left_super_zoom.webp',
@@ -84,6 +102,7 @@ let products = [
         Mid-top design
         Raised sole</p>`,
         cost: 20,
+        inCart: 1,
         image_one: 'https://product.koovs.com/131820_4889c1f558d8ded2d1f34c3cd8646fa8.webp',
         image_two: 'https://product.koovs.com/131820_b9d1a2134d3b79312649b1ea0cb37865.webp',
         image_three: 'https://product.koovs.com/131820_e0a82f2f3ac933849aa3c2ad3bf68b0f.webp',
@@ -102,6 +121,7 @@ let products = [
         Three pockets,
         Slim fit.</p>`,
         cost: 20,
+        inCart: 1,
         image_one: 'https://product.koovs.com/155246_0a8175f32abf4e7b994d2c06b6be3ba4_image1_super_zoom.webp',
         image_two: 'https://product.koovs.com/155246_0a8175f32abf4e7b994d2c06b6be3ba4_front_super_zoom.webp',
         image_three: 'https://product.koovs.com/155246_0a8175f32abf4e7b994d2c06b6be3ba4_back_super_zoom.webp',
@@ -120,6 +140,7 @@ let products = [
         Short sleeves
         Regular fit </p>`,
         cost: 20,
+        inCart: 1,
         image_one: 'https://product.koovs.com/155190_6f19357f5fb24f0d9fdc477fc3106e5a_image1_super_zoom.webp',
         image_two: 'https://product.koovs.com/155190_6f19357f5fb24f0d9fdc477fc3106e5a_front_super_zoom.webp',
         image_three: 'https://product.koovs.com/155190_6f19357f5fb24f0d9fdc477fc3106e5a_back_super_zoom.webpp',
@@ -139,6 +160,7 @@ let products = [
         Belt loops
         Slim fit</p>`,
         cost: 20,
+        inCart: 1,
         image_one: 'https://product.koovs.com/154884_75a5de1f6cc84c9f93724c080cde5065_image1_super_zoom.webp',
         image_two: 'https://product.koovs.com/154884_75a5de1f6cc84c9f93724c080cde5065_left_super_zoom.webp',
         image_three: 'https://product.koovs.com/266x230/154884_75a5de1f6cc84c9f93724c080cde5065_image1_super_zoom.jpg',
@@ -159,6 +181,7 @@ let products = [
         Long sleeves
         Slim fit</p>`,
         cost: 15,
+        inCart: 1,
         image_one: 'https://product.koovs.com/155654_a38d2a34bb7c41fa976eae46011679c3_image1_super_zoom.webp',
         image_two: 'https://product.koovs.com/266x230/155654_a38d2a34bb7c41fa976eae46011679c3_image1_super_zoom.jpg',
         image_three: 'https://product.koovs.com/266x230/155654_a38d2a34bb7c41fa976eae46011679c3_back_super_zoom.jpg',
@@ -166,61 +189,55 @@ let products = [
     },
     {
         id : 9,
-        name: 'REALM Utility Cargo Pockets Slim Trousers',
-        detail: `<p>Material/Fabric :100% Cotton</p>
+        name: 'THE SHRINE ( UV & GLOW IN THE DARK)',
+        detail: `<p>Material/Fabric :100% Cotton Biowash (180GSM)</p>
         <p>Size & Fit :
         This brand runs true to size. To ensure the best fit, we suggest consulting the size chart.</p> 
-        <p>Boots by KOOVS</p>
+        <p>THE SHRINE ( UV & GLOW IN THE DARK)</p>
         <p>
-        Made from cotton-spandex blend
-        Button and fly closure
-        Utility pockets
-        Belt loops
-        Slim fit</p>`,
-        cost: 10,
-        image_one: 'https://product.koovs.com/146x230/155265_02db3111a12c4e78a1d9eefc5dc87c54_image1_super_zoom.jpg',
-        image_two: 'https://product.koovs.com/155265_02db3111a12c4e78a1d9eefc5dc87c54_left_super_zoom.webp',
-        image_three: 'https://product.koovs.com/155265_02db3111a12c4e78a1d9eefc5dc87c54_left_super_zoom.webp',
-        image_four: 'https://product.koovs.com/146x230/155265_02db3111a12c4e78a1d9eefc5dc87c54_back_super_zoom.jpg',
+        Prints: We print using state-of-the-art screen-printing processes and other apparel embellishment techniques, including soft-hand inks, water-based inks, speciality inks, foils, and flocks. ArtBlot® is an environmentally friendly company and all of our inks are phthalate-free.
+        Wash Care: Machine wash. Wash in cold water, use mild detergent, dry in shade, do not iron directly or scrub on print, do not bleach, do not tumble dry. Dry on a flat surface as hanging may cause measurement variations.
+        Please Note: Colours may slightly vary depending on your screen brightness.</p>`,
+        cost: 35,
+        inCart: 1,
+        image_one: 'https://cdn.shopify.com/s/files/1/0073/8790/6130/products/ArtBlotMen_sBlackGlowInDarkgraphicartT-shirt_TheShrine_frontview_1800x1800.jpg?v=1594547551',
+        image_two: 'https://cdn.shopify.com/s/files/1/0073/8790/6130/products/ArtBlotMen_sBlackGlowInDarkgraphicartT-shirt_TheShrineUVtees_frontview_jpg_1800x1800.jpg?v=1594547554',
+        image_three: 'https://cdn.shopify.com/s/files/1/0073/8790/6130/products/ArtBlotMen_sBlackGlowInDarkgraphicartT-shirt_TheShrine_Closeupofgraphic_1800x1800.jpg?v=1594547558',
+        image_four: 'https://cdn.shopify.com/s/files/1/0073/8790/6130/products/ArtBlotMen_sBlackGlowInDarkgraphicartT-shirt_TheShrine_frontview_1800x1800.jpg?v=1594547551',
     },
     {
         id : 10,
-        name: 'Blue Saint Corduroy Tie-Up Cropped Trousers',
+        name: 'NEW H&M SOLID PLUS SIZE SHIRT',
         detail: `<p>Material/Fabric :100% Cotton</p>
         <p>Size & Fit :
         This brand runs true to size. To ensure the best fit, we suggest consulting the size chart.</p> 
-        <p>Trousers by BLUE SAINT  </p>
+        <p>NEW H&M SOLID PLUS SIZE SHIRT</p>
         <p>
-        Made from cotton
-        Elasticated waist 
-        Front tie-up style
-        Twin pockets 
-        All over corduroy stripes
-        Cropped length
-        Regular fit </p>`,
+        Get this H&M Solid Plus Size Shirt and more on Dexstitches, we deliver all over Nigeria.  Shop the latest trends & quality wears at the best price, we are here to help you dictate your style and express yourself with fashion pieces that let you rock your occasions and also stay in vogue.
+        </p>`,
         cost: 10,
-        image_one: 'https://product.koovs.com/155391_5948dc049baa422a8f865227769f215c_image1_super_zoom.webp',
-        image_two: 'https://product.koovs.com/155391_5948dc049baa422a8f865227769f215c_front_super_zoom.webp',
-        image_three: 'https://product.koovs.com/155391_5948dc049baa422a8f865227769f215c_back_super_zoom.webp',
-        image_four: 'https://product.koovs.com/146x230/155391_5948dc049baa422a8f865227769f215c_left_super_zoom.jpg',
+        inCart: 1,
+        image_one: 'https://dexstitches.com/image/cache/catalog/1131-1080x1440.jpg',
+        image_two: 'https://dexstitches.com/image/cache/catalog/1133-1080x1440.jpg',
+        image_three: 'https://dexstitches.com/image/cache/catalog/1132-1080x1440.jpg',
+        image_four: 'https://dexstitches.com/image/cache/catalog/1131-1080x1440.jpg',
     },
     {
         id : 11,
-        name: 'Gusto Crocskin Texture Belt',
-        detail: `<p>Material/Fabric :PU</p>
+        name: 'Men Black Full Length Jogger Track',
+        detail: `<p>Material/Fabric : Cotton Polyester</p>
         <p>Size & Fit :
-        This brand runs true to size. To ensure the best fit, we suggest consulting the size chart.</p> 
-        <p>Boots by KOOVS</p>
+        Create a look that highlight your most voguish look and keeps you comfortable by making this black jogger a part of your gym wear. It features a regular fit, stretchable waistband and a solid pattern. Make sure your exercise sessions go well and smooth by pairing it with a cozy t-shirt and shoes. Give yourself a stylish look with a futuristic wristwatch. Model's height is 6.1 ft. & he is wearing size 1.</p> 
+        <p>Men Black Full Length Jogger Track</p>
         <p>
-        Made from PU
-        Adjustable strap
-        Crocskin texture 
-        Pin & buckle fastening</p>`,
+        WASH CARE
+        Machine Wash 30 Degree Celcius, Tumble Dry Low, Wash Dark Colors Separately, Medium Iron, Do Not Bleach</p>`,
         cost: 40,
-        image_one: 'https://product.koovs.com/125916_b043fb06c629cd233bab4e08e7b5ba24.webp',
-        image_two: 'https://product.koovs.com/146x230/125916_1c2689927ae912c1eee1f09226161ad0.jpg',
-        image_three: 'https://product.koovs.com/125916_9510a0fc60859284352041fe9265bc75.webp',
-        image_four: 'https://product.koovs.com/146x230/125916_705fdaff77cadf4ebf8a58b5d2586f4d.jpg',
+        inCart: 1,
+        image_one: 'https://www.allonlinestore.in/media/product/68/73/11-wishlist.jpg',
+        image_two: 'https://www.allonlinestore.in/media/product/68/73/15-wishlist.jpg',
+        image_three: 'https://www.allonlinestore.in/media/product/68/73/14-wishlist.jpg',
+        image_four: 'https://www.allonlinestore.in/media/product/68/73/13-wishlist.jpg',
     },
     {
         id : 12,
@@ -237,6 +254,7 @@ let products = [
         Short Sleeves
         Slim Fit</p>`,
         cost: 17,
+        inCart: 1,
         image_one: 'https://product.koovs.com/155439_66b74e33b50343249b9c32d7cefccc1e_image1_super_zoom.webp',
         image_two: 'https://product.koovs.com/146x230/155439_66b74e33b50343249b9c32d7cefccc1e_front_super_zoom.jpg',
         image_three: 'https://product.koovs.com/146x230/155439_66b74e33b50343249b9c32d7cefccc1e_left_super_zoom.jpg',
@@ -277,6 +295,12 @@ function toPoductDetials(item) {
 }
 addAllProducts();
 
+function CartIsEmpty() {
+    let emptyCart = `<span class='empty-cart'>Looks Like You Haven't Added Any Product In The Cart</span>`;
+    if (cartDetails.length == 0) {
+      document.getElementsByClassName("cart-items")[0].innerHTML = emptyCart;
+    }
+  }
 function addToCart(item) {
     cartNumbers();
     let cartItems = localStorage.getItem('cartItems');
@@ -305,7 +329,7 @@ function cartNumbers() {
     productNumbers = parseInt(productNumbers)
     if (productNumbers) {
         localStorage.setItem('cartNumbers', productNumbers + 1);
-        document.querySelector("a span").textContent = productNumbers + 1
+        document.querySelector("a span").textContent = productNumbers + 1;
     } else {
         localStorage.setItem('cartNumbers', 1);
         document.querySelector("a span").textContent = 1;
@@ -387,21 +411,7 @@ function cartNumbers() {
 // ]
 
 
-var loginForm = document.getElementById("LoginForm");
-var regForm = document.getElementById("RegistrationForm");
-var Indicator = document.getElementById("Indicator");
 
-function register() {
-    regForm.style.transform = "translateX(0px)";
-    loginForm.style.transform = "translateX(0px)";
-    Indicator.style.transform = "translateX(100px)";
-}
-
-function login() {
-    regForm.style.transform = "translateX(300px)";
-    loginForm.style.transform = "translateX(300px)";
-    Indicator.style.transform = "translateX(0px)";
-}
 
 
 
